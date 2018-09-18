5ffee36429bcb99e9f5ba54146c0c7d5bd332391
Forest10 <sen.shi@yaduo.com>
2018-09-18 15:06:12 +0800

    加入增强 mybatis 注解

---
+++ src/main/java/forest10/mybatis/enhance/SelectInExtendedLanguageDriver.java
@@ -0,0 +1,34 @@
+package forest10.mybatis.enhance;
+
+import org.apache.ibatis.mapping.SqlSource;
+import org.apache.ibatis.scripting.LanguageDriver;
+import org.apache.ibatis.scripting.xmltags.XMLLanguageDriver;
+import org.apache.ibatis.session.Configuration;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 简化Select In的XMLLanguageDriver
+ *
+ * @author Forest10
+ * @date 2018/9/18 下午2:55
+ */
+public class SelectInExtendedLanguageDriver
+		extends XMLLanguageDriver implements LanguageDriver {
+
+	private final Pattern inPattern = Pattern.compile("\\(#\\{(\\w+)\\}\\)");
+
+	@Override
+	public SqlSource createSqlSource(Configuration configuration,
+	                                 String script, Class<?> parameterType) {
+
+		Matcher matcher = inPattern.matcher(script);
+		if (matcher.find()) {
+			script = matcher.replaceAll("(<foreach collection=\"$1\" item=\"__item\" separator=\",\" >#{__item}</foreach>)");
+		}
+
+		script = "<script>" + script + "</script>";
+		return super.createSqlSource(configuration, script, parameterType);
+	}
+}
