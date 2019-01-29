f4378416241f707bb6e783f8b121526ec7df13be
Forest10 <forest10@foxmail.com>
2019-01-29 16:53:52 +0800

    得到某一个目录下所有的文件

--- src/main/java/com/forest10/common/FileCmd.java
+++ src/main/java/com/forest10/common/FileCmd.java
@@ -1,8 +1,10 @@
 package com.forest10.common;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Forest10
@@ -11,24 +13,23 @@ import java.util.List;
 public class FileCmd {
 
     /**
-     * 得到某一个目录下所有的文件
-     *
+     * 得到某一个目录下所有的文件, 使用getCanonicalFile规避系统间不同
      */
-    public static List<File> getAllFile(List<File> fileList, String path,
-        List<String> excludeFileName) {
-        File file = new File(path);
-        if (!file.exists()) {
+    public static List<File> getInnerFile(List<File> fileList, File file,
+        List<String> excludeFileName) throws IOException {
+
+        if (Objects.isNull(file) || !file.exists()) {
             return Collections.emptyList();
         }
-        File[] list = file.listFiles();
-        for (File innerFile : list) {
+        File[] filesList = file.listFiles();
+        for (File innerFile : filesList) {
             if (excludeFileName.contains(innerFile.getName())) {
                 continue;
             }
             if (innerFile.isFile()) {
                 fileList.add(innerFile);
             } else {
-                getAllFile(fileList, innerFile.getAbsolutePath(), excludeFileName);
+                getInnerFile(fileList, innerFile.getCanonicalFile(), excludeFileName);
             }
 
         }
